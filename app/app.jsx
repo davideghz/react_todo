ee50c3d11472cc37de941fbe92a8fbe4cfcd60be
Davide Ghezzi <davideghz@gmail.com>
2016-07-16 14:40:41 -0400

    edit config to automatically import components

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,8 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 // same as 'var Route = require('react-route').Route;' for all elements
 
+var TodoApp = require('TodoApp');
+
 // Load foundation
 $(document).foundation();
 
@@ -11,6 +13,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-  <p>React Boilerplate</p>,
+  <TodoApp />,
   document.getElementById('app')
 );
