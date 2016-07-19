78d6168a6f1e345dfe5dc1d7e72c85b065b2022c
Davide Ghezzi <davideghz@gmail.com>
2016-07-19 11:57:54 -0400

    add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -17,5 +17,36 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+      // if return true the item stays in the array, if return false
+      // the item will be removed
+    });
+
+    // filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+      // indexOf return -1 if the text is not found, or 0 if text is found
+    });
+
+    // sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1; // a before b
+      } else if (a.completed && !b.completed) {
+        return 1; // a after b
+      } else {
+        return 0; // no changes
+      }
+    });
+
+    return filteredTodos;
   }
 };
