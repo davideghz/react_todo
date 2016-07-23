26d33b2f96424f217a2c59e7e502103a1391de70
Davide Ghezzi <davideghz@gmail.com>
2016-07-23 17:34:27 -0400

    add actions for firebase login and logout

--- app/app.jsx
+++ app/app.jsx
@@ -4,10 +4,11 @@ var {Provider} = require('react-redux');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 // same as 'var Route = require('react-route').Route;' for all elements
 
-var TodoApp = require('TodoApp');
 var actions = require('actions');
 var store = require('configureStore').configure();
 var TodoAPI = require('TodoAPI');
+import Login from 'Login';
+import TodoApp from 'TodoApp';
 
 store.dispatch(actions.startAddTodos());
 
@@ -19,7 +20,12 @@ require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
   <Provider store={store}>
-    <TodoApp/>
+    <Router history={hashHistory}>
+      <Route path="/">
+        <Route path="todos" component={TodoApp}/>
+        <IndexRoute component={Login}/>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
