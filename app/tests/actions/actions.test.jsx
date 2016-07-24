6af6eb5d2648d2f53d3b89863bb391a9dbab1739
Davide Ghezzi <davideghz@gmail.com>
2016-07-24 15:35:16 -0400

    update todos location to include uid

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -86,6 +86,25 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
+  it('should generate login action object', () => {
+    var action = {
+      type: 'LOGIN',
+      uid: '123abc'
+    };
+    const res = actions.login(action.uid);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate logout action object', () => {
+    const action = {
+      type: 'LOGOUT'
+    };
+    var res = actions.logout();
+
+    expect(res).toEqual(action);
+  });
+
   describe('Tests with firebase todos', () => {
     var testTodoRef;
 
